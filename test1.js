6d8bd98043d2a3d46d206dc1e1514b986d01b6d0
Mahesh Kumar Vankadari <mahesh.051801@gmail.com>
2026-01-09 11:22:26 +0530

    Initial Commit

---
+++ test1.js
@@ -0,0 +1,25 @@
+//1 - Temperature of a city in degrees Celsius: 25.5
+let cityTemp = 25.5
+console.log('City Temparature is ' + cityTemp + ' degrees Celsius' )
+
+//2 - Whether a customer has placed an order: true or false
+
+let customerPlacedOrder = true
+console.log(customerPlacedOrder);
+console.log(typeof customerPlacedOrder);
+
+//3 - Person's phone number: "123-456-7890"
+
+const myPhoneNumber  = '123-456-7890'
+console.log(myPhoneNumber)
+
+//4 - Amount of money in a customer's bank account: 1000.50
+
+let moneyBankAccount = 1000.50
+console.log(moneyBankAccount);
+
+//5 - Person's email address: "john.doe@example.com"
+
+const myMailId = 'john.doe@example.com';
+
+console.log (myMailId)
